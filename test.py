afd759b007131ec1a984b76b79b3571614ff1ff5
yrlee040212 <66015017+yrlee040212@users.noreply.github.com>
2024-11-04 15:03:19 +0900

    Update test.py
    주석 추가

--- test.py
+++ test.py
@@ -3,7 +3,14 @@ from ultralytics import YOLO
 from config import BEST_MODEL_PATH_TEMPLATE
 
 def test_model(plane, dataset, split="val", rect=False):
-    """학습된 모델의 성능 테스트"""
+    """학습된 모델의 성능 테스트
+    
+    Args:
+        plane (str): 평가할 평면 (예: 'axial', 'coronal', 'sagittal').
+        dataset (str): 사용할 데이터셋 이름 (예: 'base', 'preproc', 'vit_sorted').
+        split (str, optional): 평가할 데이터셋 분할 (기본값: 'val').
+        rect (bool, optional): 직사각형 평가 모드를 사용할지 여부 (기본값: False).
+    """
     best_model_path = BEST_MODEL_PATH_TEMPLATE.format(plane=plane, dataset=dataset)
     experiment_name = f"test/{plane}_{dataset}"
 
@@ -15,7 +22,7 @@ def test_model(plane, dataset, split="val", rect=False):
         save_txt=True,
         save_conf=True,
         conf=0.25 if split == "val" else None,
-        rect=rect  # 사용자 지정 rect 파라미터 사용
+        rect=rect  # 사용 여부 지정
     )
 
 if __name__ == "__main__":
